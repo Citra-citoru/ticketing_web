a0d5ab962493c9b5c091c77cf1b134469a668552
citra-citoru <citra.rahman@happy5.co>
2023-11-09 12:22:47 +0300

    add radix accordion & popover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Popover from "./component/UI/popover/popover";
+import Accordion from "./component/UI/accordion/accordion";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center py-5">
+      <h1>Build On Progress!</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,29 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        outline: "0 0 0 2px rgba(0, 0, 0, 0.9)",
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { height: "0" },
+          "100%": { height: `var(--radix-accordion-content-height)` },
+        },
+        slideUp: {
+          "0%": {
+            height: `var(--radix-accordion-content-height)`,
+          },
+          "100%": {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "slide-down": "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+        "slide-up": "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-radix")()],
 }
-
